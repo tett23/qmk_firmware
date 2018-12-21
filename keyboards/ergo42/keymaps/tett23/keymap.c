c7897909c830fc823d3e21e98ab8f8ebf1227f2e
tett23 <tett23@gmail.com>
2018-12-21 16:42:49 +0900

    Update Ergo42 keymap

--- keyboards/ergo42/keymaps/tett23/keymap.c
+++ keyboards/ergo42/keymaps/tett23/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,  KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,          XXXXXXX,   XXXXXXX,   KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,     KC_BSPC, \
-    KC_LCTL, CTL_T(KC_A), KC_S,    KC_D,    KC_F,    KC_G,          XXXXXXX,   XXXXXXX,   KC_H,    CTL_T(KC_J),  KC_K,    KC_L,    KC_SCLN,  KC_ENT, \
-    KC_LSFT, KC_Z,        KC_X,    KC_C,    KC_V,    KC_B,          XXXXXXX,   XXXXXXX,   KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT, \
-    XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_LGUI, SFT_T(KC_SPC), MO(META),  MO(META),  KC_LSFT, KC_ESC,       XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          XXXXXXX,   XXXXXXX,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          XXXXXXX,   XXXXXXX,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          XXXXXXX,   XXXXXXX,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT, \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, SFT_T(KC_SPC), MO(META),  MO(META),  KC_LSFT, KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX \
   ),
 
   /* META
